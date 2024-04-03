04f78f051b4f660ffd886c5fd2dad2061d955895
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2024-04-03 08:19:22 +0200

    Quality page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Navbar from '../src/components/Navbar/Navbar';
 import Content from './components/Header/content';
 import Services from './components/Services/Services';
-import About from "./components/About/About"
+import About from "./components/About/About";
+import Qualities from './components/Qualities/Qualities';
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
       <Content/>
       <Services/>
       <About/>
+      <Qualities/>
     </div>
   );
 }
---
+++ src/components/Qualities/Qualities.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FaPaperPlane, FaEdit, FaRocket, FaShoppingCart, FaFileAlt, FaPhoneAlt, FaEnvelopeOpen, FaMapMarkerAlt } from "react-icons/fa";
+import {AiOutlineReload} from "react-icons/ai";
+import headerMainImg from '../../assets/form.jpg';
+function Qualities() {
+    return (
+        <div className='bg-black h-auto p-24'>
+            <div className='container w-3/4  justify-center mx-auto '>
+                <div className='text-center flex flex-col lg:flex-row gap-12'>
+                    <div className='lg:w-1/2 text-white text-left'>
+                        <h4 className='text-5xl py-8 font-bold'>We Are Professionals</h4>
+                        <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Unde voluptates deserunt,
+                            molestiae soluta cumque eum.</p>
+                        <div className='flex flex-row mt-6 space-x-7'>
+                            <FaEnvelopeOpen className=' text-6xl mt-9 text-title' />
+                            <div className='text-left'>
+                                <h3 className='py-6 mt-0 text-2xl font-bold'>Ideas & Plans</h3>
+                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod.</h3>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-row mt-6 space-x-7'>
+                            <AiOutlineReload className=' text-6xl mt-9 text-title' />
+                            <div className='text-left'>
+                                <h3 className='py-6 mt-0 text-2xl font-bold'>Ideas & Plans</h3>
+                                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod.</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='lg:w-1/2'>
+                        <img src={headerMainImg} alt='about' className='w-full h-auto lg:h-full' />
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Qualities;
