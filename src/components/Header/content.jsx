76457eb4064c3e38f09fd6a639838e3f60396d86
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2024-04-19 17:47:53 +0200

    sln

--- src/components/Header/content.jsx
+++ src/components/Header/content.jsx
@@ -11,8 +11,9 @@ function App() {
         >
             <div className=' flex m-6 items-center flex-col text-center w-2/3 mt-3'>
                 <h1 className="text-title text-6xl font-black w-auto" style={{ }}>MARKETING SOLUTION AGENCY</h1>
-                <p className="text-white text-2xl mt-7">This is a simple page.Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste doloremque dolore quia omnis maxime corporis
-                    deserunt neque voluptatum.</p>
+                <p className="text-white text-2xl mt-7">
+                SolnHub is the partner of choice for many of the world’s leading enterprises, SMEs and technology challengers. We help businesses elevate their value through custom software development, product design, QA and consultancy services.
+                    </p>
                 <button type="button" className='flex items-center bg-title border-2 border-slate-600 p-3 w-36 hover:bg-slate-600 ml-10 mt-10'>
                     <IoMdRocket className='mr-2 text-white' />
                     <span className='text-white'>get started</span>
